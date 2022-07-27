c3acc660a47949f16a3aa43e2182597c3ad28d55
xenorbil <brunode2001@gmail.com>
2022-07-27 14:02:13 -0300

    version 0.1

---
+++ models/User.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor(name, gender, birth, country, email, admin, password, photo) {
+
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.admin = admin; 
+        this.password = password;
+        this.photo = photo;
+
+    }
+
+}
